4afc5acc50af6aa36f3f0658a8d133a9b180b5f2
tenkalden <tenkalden007@gmail.com>
2025-08-05 11:32:44 +0530

    fix: handle missing request context in course duration serialization

--- lms/djangoapps/mobile_api/course_info/serializers.py
+++ lms/djangoapps/mobile_api/course_info/serializers.py
@@ -9,16 +9,29 @@ from rest_framework.reverse import reverse
 
 from common.djangoapps.course_modes.models import CourseMode
 from common.djangoapps.student.models import CourseEnrollment
-from common.djangoapps.util.course import get_encoded_course_sharing_utm_params, get_link_for_about_page
-from common.djangoapps.util.milestones_helpers import get_pre_requisite_courses_not_completed
-from lms.djangoapps.courseware.access import administrative_accesses_to_course_for_user, has_access
+from common.djangoapps.util.course import (
+    get_encoded_course_sharing_utm_params,
+    get_link_for_about_page,
+)
+from common.djangoapps.util.milestones_helpers import (
+    get_pre_requisite_courses_not_completed,
+)
+from lms.djangoapps.courseware.access import (
+    administrative_accesses_to_course_for_user,
+    has_access,
+)
 from lms.djangoapps.courseware.access_utils import check_course_open_for_learner
 from lms.djangoapps.courseware.courses import get_assignments_completions
-from lms.djangoapps.mobile_api.course_info.utils import get_user_certificate_download_url
+from lms.djangoapps.mobile_api.course_info.utils import (
+    get_user_certificate_download_url,
+)
 from lms.djangoapps.mobile_api.users.serializers import ModeSerializer
 from lms.djangoapps.mobile_api.utils import get_course_organization_logo
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from openedx.features.course_duration_limits.access import get_user_course_expiration_date
+from openedx.features.course_duration_limits.access import (
+    get_user_course_expiration_date,
+)
+from lms.djangoapps.courseware.courses import get_course_about_section
 
 
 class CourseInfoOverviewSerializer(serializers.ModelSerializer):
@@ -33,7 +46,7 @@ class CourseInfoOverviewSerializer(serializers.ModelSerializer):
     is_self_paced = serializers.BooleanField(source='self_paced')
     media = serializers.SerializerMethodField()
     course_sharing_utm_parameters = serializers.SerializerMethodField()
-    course_about = serializers.SerializerMethodField('get_course_about_url')
+    course_about = serializers.SerializerMethodField("get_course_about_url")
     course_modes = serializers.SerializerMethodField()
     course_progress = serializers.SerializerMethodField()
     duration = serializers.SerializerMethodField()
@@ -63,7 +76,7 @@ class CourseInfoOverviewSerializer(serializers.ModelSerializer):
         """
         Return course images in the correct format.
         """
-        return {'image': obj.image_urls}
+        return {"image": obj.image_urls}
 
     def get_course_sharing_utm_parameters(self, obj):
         return get_encoded_course_sharing_utm_params()
@@ -76,13 +89,9 @@ class CourseInfoOverviewSerializer(serializers.ModelSerializer):
         Retrieve course modes associated with the course.
         """
         course_modes = CourseMode.modes_for_course(
-            course_overview.id,
-            only_selectable=False
+            course_overview.id, only_selectable=False
         )
-        return [
-            ModeSerializer(mode).data
-            for mode in course_modes
-        ]
+        return [ModeSerializer(mode).data for mode in course_modes]
 
     def get_course_progress(self, obj: CourseOverview) -> Dict[str, int]:
         """
@@ -93,12 +102,51 @@ class CourseInfoOverviewSerializer(serializers.ModelSerializer):
     def get_org_logo(self, course_overview):
         return get_course_organization_logo(course_overview.id)
 
+    def get_org_logo(self, course_overview):
+        return get_course_organization_logo(course_overview.id)
+
     def get_duration(self, obj):
         """
         Return course duration from about metadata.
         """
-        from lms.djangoapps.courseware.courses import get_course_about_section
-        return get_course_about_section(self.context.get('request'), obj, 'duration')
+
+        # Use the request from context or get a request with the user
+        request = self._get_request_with_user()
+
+        try:
+            # Try to get the duration using get_course_about_section
+            return get_course_about_section(request, obj, "duration")
+        except AttributeError:
+            # Handle case where CourseOverview doesn't have static_asset_path
+            # This happens because CourseOverview objects don't have all attributes of full course objects
+            try:
+                # Try to get duration from course details
+                from openedx.core.djangoapps.models.course_details import CourseDetails
+
+                course_details = CourseDetails.fetch(obj.id)
+                return course_details.duration
+            except (ImportError, AttributeError):
+                # If all else fails, return None
+                return None
+
+    def _get_request_with_user(self):
+        """
+        Returns the request from context or creates a minimal request with the user.
+        """
+        request = self.context.get("request")
+        if request is not None:
+            return request
+
+        # If no request but we have a user, create a minimal request
+        user = self.context.get("user")
+        if user is not None:
+            from django.http import HttpRequest
+
+            request = HttpRequest()
+            request.user = user
+            return request
+
+        return None
 
 
 class MobileCourseEnrollmentSerializer(serializers.ModelSerializer):
@@ -107,9 +155,9 @@ class MobileCourseEnrollmentSerializer(serializers.ModelSerializer):
     """
 
     class Meta:
-        fields = ('created', 'mode', 'is_active', 'upgrade_deadline')
+        fields = ("created", "mode", "is_active", "upgrade_deadline")
         model = CourseEnrollment
-        lookup_field = 'username'
+        lookup_field = "username"
 
 
 class CourseAccessSerializer(serializers.Serializer):
@@ -117,9 +165,11 @@ class CourseAccessSerializer(serializers.Serializer):
     Get info whether a user should be able to view course material.
     """
 
-    has_unmet_prerequisites = serializers.SerializerMethodField(method_name='get_has_unmet_prerequisites')
-    is_too_early = serializers.SerializerMethodField(method_name='get_is_too_early')
-    is_staff = serializers.SerializerMethodField(method_name='get_is_staff')
+    has_unmet_prerequisites = serializers.SerializerMethodField(
+        method_name="get_has_unmet_prerequisites"
+    )
+    is_too_early = serializers.SerializerMethodField(method_name="get_is_too_early")
+    is_staff = serializers.SerializerMethodField(method_name="get_is_staff")
     audit_access_expires = serializers.SerializerMethodField()
     courseware_access = serializers.SerializerMethodField()
 
@@ -127,31 +177,39 @@ class CourseAccessSerializer(serializers.Serializer):
         """
         Check whether or not a course has unmet prerequisites.
         """
-        return any(get_pre_requisite_courses_not_completed(data.get('user'), [data.get('course_id')]))
+        return any(
+            get_pre_requisite_courses_not_completed(
+                data.get("user"), [data.get("course_id")]
+            )
+        )
 
     def get_is_too_early(self, data: dict) -> bool:
         """
         Determine if the course is open to a learner (course has started or user has early beta access).
         """
-        return not check_course_open_for_learner(data.get('user'), data.get('course'))
+        return not check_course_open_for_learner(data.get("user"), data.get("course"))
 
     def get_is_staff(self, data: dict) -> bool:
         """
         Determine whether a user has staff access to this course.
         """
-        return any(administrative_accesses_to_course_for_user(data.get('user'), data.get('course_id')))
+        return any(
+            administrative_accesses_to_course_for_user(
+                data.get("user"), data.get("course_id")
+            )
+        )
 
     def get_audit_access_expires(self, data: dict) -> Union[str, None]:
         """
         Returns expiration date for a course audit expiration, if any or null
         """
-        return get_user_course_expiration_date(data.get('user'), data.get('course'))
+        return get_user_course_expiration_date(data.get("user"), data.get("course"))
 
     def get_courseware_access(self, data: dict) -> dict:
         """
         Determine if the learner has access to the course, otherwise show error message.
         """
-        return has_access(data.get('user'), 'load_mobile', data.get('course')).to_json()
+        return has_access(data.get("user"), "load_mobile", data.get("course")).to_json()
 
 
 class CourseDetailSerializer(serializers.Serializer):
@@ -173,7 +231,7 @@ class CourseDetailSerializer(serializers.Serializer):
         """
         Returns course id.
         """
-        return str(data['course_id'])
+        return str(data["course_id"])
 
     @staticmethod
     def get_course_overview(course_id):
@@ -186,29 +244,35 @@ class CourseDetailSerializer(serializers.Serializer):
         """
         Returns course info overview.
         """
-        course_overview = self.get_course_overview(data['course_id'])
-        course_info_context = {'user': data['user']}
-        return CourseInfoOverviewSerializer(course_overview, context=course_info_context).data
+        course_overview = self.get_course_overview(data["course_id"])
+        course_info_context = {"user": data["user"]}
+        return CourseInfoOverviewSerializer(
+            course_overview, context=course_info_context
+        ).data
 
     @staticmethod
     def get_discussion_url(data):
         """
         Returns discussion url.
         """
-        course_overview = CourseOverview.get_from_id(data['course_id'])
-        if not course_overview.is_discussion_tab_enabled(data['user']):
+        course_overview = CourseOverview.get_from_id(data["course_id"])
+        if not course_overview.is_discussion_tab_enabled(data["user"]):
             return
 
-        return reverse('discussion_course', kwargs={'course_id': data['course_id']}, request=data['request'])
+        return reverse(
+            "discussion_course",
+            kwargs={"course_id": data["course_id"]},
+            request=data["request"],
+        )
 
     def get_course_access_details(self, data):
         """
         Returns course access details.
         """
         course_access_data = {
-            'course': self.get_course_overview(data['course_id']),
-            'course_id': data['course_id'],
-            'user': data['user'],
+            "course": self.get_course_overview(data["course_id"]),
+            "course_id": data["course_id"],
+            "user": data["user"],
         }
         return CourseAccessSerializer(course_access_data).data
 
@@ -217,14 +281,18 @@ class CourseDetailSerializer(serializers.Serializer):
         """
         Returns course certificate url.
         """
-        return get_user_certificate_download_url(data['request'], data['user'], data['course_id'])
+        return get_user_certificate_download_url(
+            data["request"], data["user"], data["course_id"]
+        )
 
     @staticmethod
     def get_enrollment_details(data):
         """
         Retrieve course enrollment details of the course.
         """
-        user_enrollment = CourseEnrollment.get_enrollment(user=data['user'], course_key=data['course_id'])
+        user_enrollment = CourseEnrollment.get_enrollment(
+            user=data["user"], course_key=data["course_id"]
+        )
         return MobileCourseEnrollmentSerializer(user_enrollment).data
 
     @staticmethod
@@ -233,13 +301,23 @@ class CourseDetailSerializer(serializers.Serializer):
         Returns course_handouts.
         """
 
-        url_params = {'api_version': data['api_version'], 'course_id': data['course_id']}
-        return reverse('course-handouts-list', kwargs=url_params, request=data['request'])
+        url_params = {
+            "api_version": data["api_version"],
+            "course_id": data["course_id"],
+        }
+        return reverse(
+            "course-handouts-list", kwargs=url_params, request=data["request"]
+        )
 
     @staticmethod
     def get_course_updates(data):
         """
         Returns course_updates.
         """
-        url_params = {'api_version': data['api_version'], 'course_id': data['course_id']}
-        return reverse('course-updates-list', kwargs=url_params, request=data['request'])
+        url_params = {
+            "api_version": data["api_version"],
+            "course_id": data["course_id"],
+        }
+        return reverse(
+            "course-updates-list", kwargs=url_params, request=data["request"]
+        )
