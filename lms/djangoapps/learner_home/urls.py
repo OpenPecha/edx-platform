d82bb3a3010c4bc4b4eb96e16554d690f23ff93d
Yogesh Bhagat <yogeshbhagat42code@gmail.com>
2025-10-16 14:28:11 +0530

    add api for fetching course progress on dashboard

--- lms/djangoapps/learner_home/urls.py
+++ lms/djangoapps/learner_home/urls.py
@@ -14,6 +14,7 @@ app_name = "learner_home"
 # Learner Dashboard Routing
 urlpatterns = [
     re_path(r"^init/?", views.InitializeView.as_view(), name="initialize"),
+    re_path(r"^completion_data/?", views.CompletionSummaryView.as_view(), name="completion_summary"),
     path("mock/", include("lms.djangoapps.learner_home.mock.urls")),
     path("", include(rest_api_urls)),
 ]
